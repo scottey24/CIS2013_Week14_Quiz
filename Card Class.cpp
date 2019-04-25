9b74c29101844db6d2b9e2f841e1d2a633340508
Scott Culp <scot.r.culp@hotmail.com>
2019-04-24 20:15:58 -0500

    added weight

--- Card Class.cpp
+++ Card Class.cpp
@@ -13,12 +13,32 @@ class Card{
 		int weight;
 		bool is used;
 		
+		void set_weight (char w) {
+			switch(w){
+				case'A':
+				 weight = 11;
+					break;
+				 case 'J':
+				 weight = 10;
+					break;
+				 case 'Q':
+				 weight = 10;
+					break;
+				case 'K':
+				weight = 10;
+					break;
+					
+				default;
+					weight = (int)w;
+		
 		public;
 		//setters
 		void set_suit(string s) {suite = s;}
-		void set_value(char v0 {value = v;}
+		void set_value(char v) {
+			value = v
 		void set_used(bool u) {is_used = u;}
-		void set_weight (int w) {weight = w;}
+		
+		
 		// getters
 		string get_suite() { return suite;}
 		char get_value () {return value;}
