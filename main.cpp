52f3031fd48ee1bd890417e34fc6800774e2a236
Scott Culp <scot.r.culp@hotmail.com>
2019-04-24 19:35:08 -0500

    Update main.cpp
    created 2 users and give names

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <string>
-#include 
+#include "player.cpp"
 
 using namespace std;
 int main (){
 	string player 1;
+	char hors ='h';
+	Person dealer;
+	Person player;
+	dealer.set_name("Dealer");
 	
 	cout << "Enter the name of yhour player:";
 	cin >> player1;
+	player.set_name(player1);
 	
-	cout << " dealer has a diamond A, and a club  5"<< endl;
-	cout << player1>> "has a spade A, and a Heart K" << endl;
+	cout << " dealer has a A of diamond, and a 5 of club"<< endl;
+	cout << player1>> "has a A of spades , and a K of Heart" << endl;
 	cout << "(h)it or (s)tay;";
-	cin >> HorS;
+	cin >> hOrs
+	palyer.set_hitting(hOrs);
 	
 	return 0
 }
