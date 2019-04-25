886909d5f704cc0337f6ce9234bbc22603cf543d
Scott Culp <scot.r.culp@hotmail.com>
2019-04-24 19:18:40 -0500

    Update player class.cpp
    person and card classes created and compiling

--- player class.cpp
+++ player class.cpp
@@ -1,7 +1,26 @@
-#include <cstdlib>
-#include <time.h>
-#include <iostream>
+#include <string>
+#include "Card.cpp"
+
 
 using namespace std;
-	private:
+
+class Person {
+	
+	private;
+	
+	string name;
+	bool is_hitting;
+	Card hand[9];
+	
+	public;
+	
+		Person (){
+			set_name("unkinown);
+			{
+			}
+			void set_name(string n) ({name = n;}
+			void set_hitting (bool h) {is_hitting = h;}
+			
 		
+		}
+};
